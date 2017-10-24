ce8904bbd86d5aba9c3bf31b202331cff593ef2d
Analin Flores <foglzerika@gmail.com>
2017-10-24 14:25:25 -0500

    Create function loadPage, create array of users and create class User

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// Array of users
+const users=[{name:"Alba", email:"alba@dive.a",password:"alba2017!"}];
+// Constructor user
+const User =class{
+    constructor(name,email,password){
+      this.name=name;
+      this.email=email;
+      this.password=password;
+    }
+}
+
 const loadPAge = ()=>{
   const btnLogin= document.getElementById('go');
   const newUser = document.getElementById("addUser");
